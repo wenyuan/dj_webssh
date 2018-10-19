c7374aa2a8919d0d09a078ab3da34c85e98be548
wenyuan <xuwenyuan2016@163.com>
2018-10-19 14:07:27 +0800

    :fire: reconstruct

--- django_webssh/settings.py
+++ django_webssh/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webssh'
+    'apps.webssh'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'django_webssh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,4 +122,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'webssh/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
