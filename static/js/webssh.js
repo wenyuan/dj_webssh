5dcc9f5709d4d82060e8fbaf09269c7dccf71582
wenyuan <xuwenyuan2016@163.com>
2018-12-27 17:33:18 +0800

    :sparkles: add heartbeat check

--- static/js/webssh.js
+++ static/js/webssh.js
@@ -1,62 +1,80 @@
 /**
- * Created by xwenyuan on 2018/10/15.
+ * Created by winyuan on 2018/10/15.
  */
 
-function  WebSSHClient() {
-
-};
-
+function WebSSHClient() {
+    this.heartBeatInterval = 3000;
+    this.heartBeatTimer = null;
+    this.serverTimer = null;
+}
 
 WebSSHClient.prototype._generateURL = function (options) {
-    if (window.location.protocol == 'https:'){
-        var protocol = 'wss://';
-    }else{
-        var protocol = 'ws://';
-    }
-    // ws://192.168.1.108:8000/host/3/
-    var url = protocol + window.location.host + '/host/' + encodeURIComponent(options.des_id) + '/';
-    return url;
+    let protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+    let url = protocol + '://' + window.location.host + '/host/' + encodeURIComponent(options.des_id) + '/';
+    return url
 };
 
 
 WebSSHClient.prototype.connect = function (options) {
     // ws://192.168.1.108:8000/host/3/
-    var des_url = this._generateURL(options);
+    let desUrl = this._generateURL(options);
+    let self = this;
 
     if (window.WebSocket) {
-        this._connection = new WebSocket(des_url);
+        this._connection = new WebSocket(desUrl);
     }
     else if (window.MozWebSocket) {
-        this._connection = new MozWebSocket(des_url);
+        this._connection = new MozWebSocket(desUrl);
     }
     else {
         options.onError('当前浏览器不支持WebSocket！');
-        return ;
+        return;
     }
-    
+
     this._connection.onopen = function () {
-        options.onConnect();        
+        options.onConnect();
+        self.stopHeartBeatCheck().startHeartBeatCheck()
     };
-    
+
     this._connection.onmessage = function (evt) {
-        var data = JSON.parse(evt.data.toString());
+        let data = JSON.parse(evt.data.toString());
         if (data.error !== undefined) {
             options.onError(data.error);
-        }
-        else {
+        } else if (data.data === 'heart beat check...') {
+        } else {
             options.onData(data.data);
         }
+        self.stopHeartBeatCheck().startHeartBeatCheck()
     };
 
     this._connection.onclose = function (evt) {
+        self.stopHeartBeatCheck();
         options.onClose();
     };
 };
 
 WebSSHClient.prototype.send = function (data) {
-    this._connection.send(JSON.stringify({'data':data}));
+    this._connection.send(JSON.stringify({'data': data}));
 };
 
 WebSSHClient.prototype.close = function () {
-  this._connection.close()
+    this._connection.close()
+};
+
+WebSSHClient.prototype.startHeartBeatCheck = function () {
+    let self = this;
+    if (this._connection.readyState === this._connection.OPEN) {
+        this.heartBeatTimer = window.setInterval(function () {
+            self._connection.send(JSON.stringify({'data': 'heart beat check...'}))
+            self.serverTimer = window.setInterval(function () {
+                self._connection.close()
+            }, self.heartBeatInterval)
+        }, this.heartBeatInterval)
+    }
+};
+
+WebSSHClient.prototype.stopHeartBeatCheck = function () {
+    window.clearInterval(this.heartBeatTimer);
+    window.clearInterval(this.serverTimer);
+    return this
 };
